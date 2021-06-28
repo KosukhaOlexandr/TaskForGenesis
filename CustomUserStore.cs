9175e3cb5cfc89aab0ec406f9eeae7c6901a7427
Olexandr Kosukha <55558468+KosukhaOlexandr@users.noreply.github.com>
2021-06-28 23:52:58 +0300

    Add files via upload

---
+++ CustomUserStore.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+using Microsoft.AspNetCore.Identity;
+using TaskForGenesis.Models;
+
+namespace TaskForGenesis.CustomProvider
+{
+    public class CustomUserStore : IUserStore<User>, IUserPasswordStore<User>
+    {
+        private XmlUsersTable _usersTable;
+
+        public CustomUserStore(XmlUsersTable usersTable)
+        {
+            _usersTable = usersTable;
+        }
+
+        public async Task<IdentityResult> CreateAsync (User user,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (user == null) throw new ArgumentNullException(nameof(user));
+
+            return  _usersTable.Create(user);
+        }
+
+        public Task<IdentityResult> UpdateAsync (User user,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> DeleteAsync(User user,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> FindByIdAsync(string userId,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (userId == null) throw new ArgumentNullException(nameof(userId));
+            Guid idGuid;
+            if (!Guid.TryParse(userId, out idGuid))
+            {
+                throw new ArgumentException("Not a valid Guid id", nameof(userId));
+            }
+
+            return  _usersTable.FindById(idGuid);
+        }
+
+        public async Task<User> FindByNameAsync(string userName,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (userName == null) throw new ArgumentNullException(nameof(userName));
+
+            return _usersTable.FindByName(userName);
+        }
+
+        public Task<string> GetUserIdAsync(User user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (user == null) throw new ArgumentNullException(nameof(user));
+
+            return Task.FromResult(user.Id.ToString());
+        }
+
+        public Task<string> GetUserNameAsync(User user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (user == null) throw new ArgumentNullException(nameof(user));
+
+            return Task.FromResult(user.UserName);
+        }
+
+        public Task SetUserNameAsync(User user, string userName, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (user == null) throw new ArgumentNullException(nameof(user));
+            if (userName == null) throw new ArgumentNullException(nameof(user));
+
+            user.UserName = userName;
+            return Task.FromResult<object>(null);
+        }
+
+        public Task SetNormalizedUserNameAsync(User user, string normalizedUserName, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (user == null) throw new ArgumentNullException(nameof(user));
+            if (normalizedUserName == null) throw new ArgumentNullException(nameof(user));
+
+            user.NormalizedUserName = normalizedUserName;
+            return Task.FromResult<object>(null);
+        }
+
+        public Task SetPasswordHashAsync(User user, string passwordHash, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (user == null) throw new ArgumentNullException(nameof(user));
+            if (passwordHash == null) throw new ArgumentNullException(nameof(passwordHash));
+
+            user.PasswordHash = passwordHash;
+            return Task.FromResult<object>(null);
+        }
+
+        public Task<string> GetPasswordHashAsync(User user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (user == null) throw new ArgumentNullException(nameof(user));
+
+            return Task.FromResult(user.PasswordHash);
+        }
+        public Task<bool> HasPasswordAsync(User user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public Task<string> GetNormalizedUserNameAsync(User user,
+           CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
