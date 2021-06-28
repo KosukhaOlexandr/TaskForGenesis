e2231e07a5747ad5159b00071f78b9922a449cb9
Olexandr Kosukha <55558468+KosukhaOlexandr@users.noreply.github.com>
2021-06-28 23:49:10 +0300

    Add files via upload

---
+++ ViewModel/RegisterViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+namespace TaskForGenesis.BiewModel
+{
+    public class RegisterViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Паролі не співпадають")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirm { get; set; }
+
+    }
+}
